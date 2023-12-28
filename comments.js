346360cf5e23ca3295e1d4fe45bdfef86c1502c1
Chathura Prabashana <prabashana.dev@gmail.com>
2023-12-28 06:54:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,73 @@
-// create web server
+// Create web server
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const axios = require('axios');
 
-// 1. load module
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
+// Create web server
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// 2. create server
-http.createServer(function(request, response) {
-    // 3. get url
-    var urlObj = url.parse(request.url);
-    var pathName = urlObj.pathname;
+// Create comments array
+const commentsByPostId = {};
 
-    // 4. read file
-    if (pathName == '/') {
-        showIndex(response);
-    } else if (pathName == '/list') {
-        showList(response);
-    } else if (pathName == '/image.png') {
-        showImage(response);
-    } else if (pathName == '/upload' && request.method == 'POST') {
-        uploadFile(request, response);
-    } else if (pathName.indexOf('/upload?') >= 0 && request.method == 'GET') {
-        show404(response);
-    } else {
-        var filePath = path.join(__dirname, pathName);
-        fs.readFile(filePath, function(err, data) {
-            if (err) {
-                console.log(err);
-                response.end('404 Not Found.');
-            }
-            response.end(data);
+// Create route to get comments
+app.get('/posts/:id/comments', (req, res) => {
+    // Send comments
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+// Create route to post comments
+app.post('/posts/:id/comments', async (req, res) => {
+    // Create comment id
+    const commentId = randomBytes(4).toString('hex');
+    // Get comment content
+    const { content } = req.body;
+    // Get post id
+    const postId = req.params.id;
+    // Get comments of post
+    const comments = commentsByPostId[postId] || [];
+    // Push new comment to comments
+    comments.push({ id: commentId, content, status: 'pending' });
+    // Update comments
+    commentsByPostId[postId] = comments;
+    // Send event to event bus
+    await axios.post('http://event-bus-srv:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId,
+            status: 'pending'
+        }
+    });
+    // Send comments
+    res.status(201).send(comments);
+});
+
+// Create route to receive event
+app.post('/events', async (req, res) => {
+    // Get event type
+    const { type, data } = req.body;
+    // Check event type
+    if (type === 'CommentModerated') {
+        // Get comment id
+        const { id, postId, status, content } = data;
+        // Get comments of post
+        const comments = commentsByPostId[postId];
+        // Get comment to update
+        const comment = comments.find(comment => {
+            return comment.id === id;
         });
-    }
-}).listen(8080);
-
-console.log('Server is running at http://
+        // Update comment status
+        comment.status = status;
+        // Send event to event bus
+        await axios.post('http://event-bus-srv:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
